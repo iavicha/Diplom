117a9625621c7bff155403f3962c62c4910b6c81
ilia <ivetchinin@gmail.com>
2021-02-08 01:23:54 +0300

    make folders for templates

---
+++ login/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
+
+
+# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Вход выполнен')
+            return redirect('index')
+        else:
+            messages.error(request, 'Вход не выполнен')
+            return redirect('login')
+
+    else:
+
+        return render(request, 'login/login.html')
